ccca18dee682625f2085e8d5f3fdf0cd119374a6
Vaishnavi <vaishnavi.kammara1@gmail.com>
2026-02-07 19:42:26 +0530

    Initial commit

---
+++ src/test/java/com/restful_webservices/rest_apis/RestApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful_webservices.rest_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
